d0298f1376f7780a4e87df69d316dd622bc52313
bowie <leticia.bini@gmail.com>
2023-10-02 18:56:54 -0300

    commit

--- lib/engine/Game.dart
+++ lib/engine/Game.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Game(),
-      ),
-    );
-  }
-}
-
 class Game extends StatefulWidget {
   @override
   _GameState createState() => _GameState();
